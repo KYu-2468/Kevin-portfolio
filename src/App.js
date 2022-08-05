e96f683ff8442445ae4d0b0503665bccea8ee4d3
Kevin Yu <Kevin.Yu.2468@gmail.com>
2022-08-04 18:18:53 -0700

    Add hashrouter and setup routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { Home, About, Portfolios } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <div>This is app test!</div>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <HashRouter>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/portfolios" element={<Portfolios />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import About from "./About";
+import Portfolios from "./Portfolios";
+
+export { Home };
+export { About };
+export { Portfolios };
