1ac05b758938b14599b18f4e1d9625888410e051
Kevin Yu <Kevin.Yu.2468@gmail.com>
2022-08-05 20:18:50 -0700

    Update format of about-intro section

--- src/sections/About.js
+++ src/sections/About.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const About = () => (
   <div id="about" className="section">
-    <p> Hi, I'm</p>
-    <h1>Kevin</h1>
-    <p>A software engineer</p>
+    <div className="intro">
+      <p> Hi, I'm</p>
+      <h1>Kevin</h1>
+      <p>A software engineer</p>
+    </div>
+    <div className="summary"></div>
   </div>
 );
 
